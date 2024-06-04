496a7ea0391e7c53d3888c7814f1f65c81ed58d0
jinwoong <baseball0020@gmail.com>
2024-06-04 13:24:50 +0900

    test: 가짜 객체 생성

---
+++ TodaysFlowers/Preview Content/UseCaseStubs/SearchUseCaseStub.swift
@@ -0,0 +1,27 @@
+//
+//  SearchUseCaseStub.swift
+//  TodaysFlowers
+//
+//  Created by jinwoong Kim on 6/4/24.
+//
+
+import Foundation
+import Combine
+
+final class SearchUseCaseStub: SearchUseCase {
+    func searchBy(name: String) -> AnyPublisher<[Flower], Never> {
+        Just(FlowerStubs.flowers)
+            .map { flowers in
+                flowers.filter { $0.name == name }
+            }
+            .eraseToAnyPublisher()
+    }
+    
+    func searchBy(flowerLang: String) -> AnyPublisher<[Flower], Never> {
+        Just(FlowerStubs.flowers)
+            .map { flowers in
+                flowers.filter { $0.lang == flowerLang }
+            }
+            .eraseToAnyPublisher()
+    }
+}
