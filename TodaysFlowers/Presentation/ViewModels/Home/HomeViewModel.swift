4cc1d95211675a4b2f44096ced40cb57a347e64f
mghhwang <freemin217@gmail.com>
2024-06-10 00:46:07 +0900

    feat: HomeViewModel 추가

---
+++ TodaysFlowers/Presentation/ViewModels/Home/HomeViewModel.swift
@@ -0,0 +1,45 @@
+//
+//  HomeViewModel.swift
+//  TodaysFlowers
+//
+//  Created by 황민경 on 6/10/24.
+//
+
+import UIKit
+import Combine
+import Foundation
+
+class HomeViewModel: ObservableObject {
+    @Published var flowers: [Flower] = []
+    
+    init() {
+        loadFlowers()
+    }
+    
+    func loadFlowers() {
+        flowers = [
+            Flower(
+                id: 1,
+                name: "아젤레아",
+                lang: "사랑의 기쁨",
+                content: "Some content",
+                type: "Some type",
+                grow: "Some grow information",
+                usage: "Some usage",
+                imageData: [UIImage.assetToData("sample_flower1.jpeg")], // Some image data
+                date: Date()
+            ),
+            Flower(
+                id: 15,
+                name: "온시디움",
+                lang: "순박한 마음",
+                content: "Some content",
+                type: "Some type",
+                grow: "Some grow information",
+                usage: "Some usage",
+                imageData: [UIImage.assetToData("sample_flower4.jpeg")], // Some image data
+                date: Date()
+            )
+        ]
+    }
+}
