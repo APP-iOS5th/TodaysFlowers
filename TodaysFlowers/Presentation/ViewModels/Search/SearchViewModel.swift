ab88051c7f3d077c168bd150bd8773ab90201db8
seonggeun an <eventscripts@nate.com>
2024-06-12 04:23:54 +0900

    feat: SearchView 파일 추가

---
+++ TodaysFlowers/Presentation/ViewModels/Search/SearchViewModel.swift
@@ -0,0 +1,42 @@
+//
+//  SearchViewModel.swift
+//  TodaysFlowers
+//
+//  Created by 이인호 on 6/4/24.
+//
+
+import Foundation
+import Combine
+
+class SearchViewModel {
+    private let useCase: any SearchUseCase
+    @Published var flowers: [Flower] = []
+    @Published var searchType: SearchType = .name
+    private var searchTextPublisher = PassthroughSubject<String, Never>()
+    
+    init(useCase: any SearchUseCase = FlowersApi()) {
+        self.useCase = useCase
+        setupSearchPublisher()
+    }
+    
+    private func setupSearchPublisher() {
+        searchTextPublisher
+            .debounce(for: .milliseconds(500), scheduler: RunLoop.main)
+            .removeDuplicates()
+            .flatMap { [weak self] searchText -> AnyPublisher<[Flower], Never> in
+                guard let self = self else { return Just([]).eraseToAnyPublisher() }
+                switch self.searchType {
+                case .name:
+                    return self.useCase.searchBy(name: searchText)
+                case .flowerLang:
+                    return self.useCase.searchBy(flowerLang: searchText)
+                }
+            }
+            .assign(to: &$flowers) // Combine의 re-publish 기능
+    }
+    
+    func search(inputText: String) {
+        searchTextPublisher.send(inputText)
+    }
+}
+
---
+++ TodaysFlowers/Presentation/Views/Search/SearchTableViewCell.swift
@@ -0,0 +1,72 @@
+//
+//  SearchTableViewCell.swift
+//  TodaysFlowers
+//
+//  Created by 이인호 on 6/4/24.
+//
+
+import UIKit
+
+final class SearchTableViewCell: UITableViewCell {
+    
+    static let identifier = "flowerCell"
+    
+    private lazy var thumbnailView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let nameLabel = UILabel()
+        nameLabel.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return nameLabel
+    }()
+    
+    private lazy var flowerLangLabel: UILabel = {
+        let flowerLangLabel = UILabel()
+        flowerLangLabel.font = UIFont.systemFont(ofSize: 17, weight: .regular)
+        flowerLangLabel.textColor = .gray
+        return flowerLangLabel
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(thumbnailView)
+        addSubview(nameLabel)
+        addSubview(flowerLangLabel)
+        
+        thumbnailView.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        flowerLangLabel.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = safeAreaLayoutGuide
+        let marginGuide = layoutMarginsGuide
+        
+        NSLayoutConstraint.activate([
+            thumbnailView.topAnchor.constraint(equalTo: safeArea.topAnchor),
+            thumbnailView.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor),
+            thumbnailView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 8),
+            thumbnailView.widthAnchor.constraint(equalToConstant: 90),
+            
+            nameLabel.topAnchor.constraint(equalTo: marginGuide.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: thumbnailView.trailingAnchor, constant: 8),
+            nameLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor, constant: -8),
+            
+            flowerLangLabel.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 8),
+            flowerLangLabel.leadingAnchor.constraint(equalTo: thumbnailView.trailingAnchor, constant: 8),
+            flowerLangLabel.trailingAnchor.constraint(equalTo: marginGuide.trailingAnchor, constant: -8),
+        ])
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configureCell(flower: Flower) {
+        thumbnailView.image = UIImage(data: flower.imageData[0])
+        nameLabel.text = flower.name
+        flowerLangLabel.text = flower.lang
+    }
+}
