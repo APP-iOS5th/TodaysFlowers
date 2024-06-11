182d58fa6ea3de708c12b3ce2d10e34feca1badf
seonggeun an <eventscripts@nate.com>
2024-06-12 04:26:11 +0900

    feat: SearchViewUseCase 함수 구현( 테스트필요)

--- TodaysFlowers/Network/FlowersApi.swift
+++ TodaysFlowers/Network/FlowersApi.swift
@@ -9,12 +9,20 @@ import Combine
 import Foundation
 import XMLCoder
 
+
 final class FlowersApi: DetailViewUseCase, SearchUseCase  {
     
     let apikey = "FByld3CLZFH7McV%2B9rrh55h%2F0vqtm0im9vy8Cl16wQOU57mFDm6KkfdwmbW%2FdovSFTlH0KFSroxN7XjFLnIyKg%3D%3D"
     
     func getFlower(by id: Int) -> AnyPublisher<Flower, Never> {
         let urlString = "https://apis.data.go.kr/1390804/NihhsTodayFlowerInfo01/selectTodayFlowerView01?serviceKey=\(apikey)&dataNo=\(id)"
+        let urlString = EndpointFactory.buildEndpoint(
+            with: .detail,
+            keyValue: [
+                "serviceKey": apikey,
+                "dataNo": String(id),
+            ]
+        )
         
         guard let url = URL(string: urlString) else {
             fatalError("Invalid URL")
@@ -25,6 +33,7 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                    .decode(type: Document.self, decoder: XMLDecoder())
                    .map { document -> Flower in
                        let result = document.root.result
+                       let result = document.root.result.first!
                        let imageUrls = [result.imgUrl1, result.imgUrl2, result.imgUrl3]
                        var imageData: [Data] = []
                        
@@ -51,6 +60,10 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                                      type: result.fType,
                                      grow: result.fGrow,
                                      usage: result.fUse,
+                                     content: result.fContent ?? "",
+                                     type: result.fType ?? "",
+                                     grow: result.fGrow ?? "",
+                                     usage: result.fUse ?? "",
                                      imageData: imageData,
                                      date: date)
                    }
@@ -64,6 +77,16 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
         let urlString =
         "https://apis.data.go.kr/1390804/NihhsTodayFlowerInfo01/selectTodayFlowerList01?serviceKey=\(apikey)&pageNo=1&numOfRows=100&searchType=1&searchWord=\(name)"
         
+        let urlString = EndpointFactory.buildEndpoint(
+            with: .list,
+            keyValue: [
+                "serviceKey": apikey,
+                "pageNo": "1",
+                "numOfRows": "10",
+                "searchType": "1",
+                "searchWord": name.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+            ]
+        )
         guard let url = URL(string: urlString) else {
             fatalError("Invalid URL")
         }
@@ -71,6 +94,7 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
         return URLSession.shared.dataTaskPublisher(for: url)
              .map(\.data)
              .decode(type: Document.self, decoder: XMLDecoder())
+//             .print()
              .map { document -> [Flower] in
                  let result = document.root.result
                  
@@ -82,6 +106,18 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                      guard let url = URL(string: imageUrl),
                            let data = try? Data(contentsOf: url) else {
                          fatalError("Failed to load image data from URL: \(imageUrl)")
+                let results = document.root.result
+                return results.map{ result -> Flower in
+                     // Convert image URLs to Data
+                     let imageUrls = [result.imgUrl1, result.imgUrl2, result.imgUrl3]
+                     var imageData: [Data] = []
+                     
+                     for imageUrl in imageUrls {
+                         guard let url = URL(string: imageUrl),
+                               let data = try? Data(contentsOf: url) else {
+                             fatalError("Failed to load image data from URL: \(imageUrl)")
+                         }
+                         imageData.append(data)
                      }
                      imageData.append(data)
                  }
@@ -92,6 +128,15 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                  let dateString = "\(result.fMonth)-\(result.fDay)"
                  guard let date = dateFormatter.date(from: dateString) else {
                      fatalError("Failed to convert date: \(dateString)")
+                     
+                     // Convert fMonth and fDay to Date
+                     let dateFormatter = DateFormatter()
+                     dateFormatter.dateFormat = "MM-dd"
+                     let dateString = "\(result.fMonth)-\(result.fDay)"
+                     guard let date = dateFormatter.date(from: dateString) else {
+                         fatalError("Failed to convert date: \(dateString)")
+                     }
+                     return Flower(id: Int(result.dataNo)!, name: result.flowNm, lang: result.flowLang, content: result.fContent ?? "", type: result.fType ?? "", grow: result.fGrow ?? "", usage: result.fUse ?? "", imageData: imageData, date: date)
                  }
                  
                  let flower = Flower(id: Int(result.dataNo) ?? 0,
@@ -106,6 +151,7 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                  return [flower]
              }
              .replaceError(with: [])
+             .replaceError(with: [Flower(id: -1, name: "", lang: "", content: "", type: "", grow: "", usage: "", imageData: [], date: Date())])
              .eraseToAnyPublisher()
      }
     
@@ -115,6 +161,16 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
         let urlString =
         "https://apis.data.go.kr/1390804/NihhsTodayFlowerInfo01/selectTodayFlowerList01?serviceKey=\(apikey)&pageNo=1&numOfRows=100&searchType=4&searchWord=\(flowerLang)"
         
+        let urlString = EndpointFactory.buildEndpoint(
+            with: .list,
+            keyValue: [
+                "serviceKey": apikey,
+                "pageNo": "1",
+                "numOfRows": "10",
+                "searchType": "4",
+                "searchWord": flowerLang.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed)!
+            ]
+        )
         guard let url = URL(string: urlString) else {
             fatalError("Invalid URL")
         }
@@ -122,6 +178,7 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
         return URLSession.shared.dataTaskPublisher(for: url)
              .map(\.data)
              .decode(type: Document.self, decoder: XMLDecoder())
+//             .print()
              .map { document -> [Flower] in
                  let result = document.root.result
                  
@@ -133,6 +190,18 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                      guard let url = URL(string: imageUrl),
                            let data = try? Data(contentsOf: url) else {
                          fatalError("Failed to load image data from URL: \(imageUrl)")
+                let results = document.root.result
+                return results.map{ result -> Flower in
+                     // Convert image URLs to Data
+                     let imageUrls = [result.imgUrl1, result.imgUrl2, result.imgUrl3]
+                     var imageData: [Data] = []
+                     
+                     for imageUrl in imageUrls {
+                         guard let url = URL(string: imageUrl),
+                               let data = try? Data(contentsOf: url) else {
+                             fatalError("Failed to load image data from URL: \(imageUrl)")
+                         }
+                         imageData.append(data)
                      }
                      imageData.append(data)
                  }
@@ -143,6 +212,15 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                  let dateString = "\(result.fMonth)-\(result.fDay)"
                  guard let date = dateFormatter.date(from: dateString) else {
                      fatalError("Failed to convert date: \(dateString)")
+                     
+                     // Convert fMonth and fDay to Date
+                     let dateFormatter = DateFormatter()
+                     dateFormatter.dateFormat = "MM-dd"
+                     let dateString = "\(result.fMonth)-\(result.fDay)"
+                     guard let date = dateFormatter.date(from: dateString) else {
+                         fatalError("Failed to convert date: \(dateString)")
+                     }
+                     return Flower(id: Int(result.dataNo)!, name: result.flowNm, lang: result.flowLang, content: result.fContent ?? "", type: result.fType ?? "", grow: result.fGrow ?? "", usage: result.fUse ?? "", imageData: imageData, date: date)
                  }
                  
                  let flower = Flower(id: Int(result.dataNo) ?? 0,
@@ -157,6 +235,7 @@ final class FlowersApi: DetailViewUseCase, SearchUseCase  {
                  return [flower]
              }
              .replaceError(with: [])
+             .replaceError(with: [Flower(id: -1, name: "", lang: "", content: "", type: "", grow: "", usage: "", imageData: [], date: Date())])
              .eraseToAnyPublisher()
      }
     
