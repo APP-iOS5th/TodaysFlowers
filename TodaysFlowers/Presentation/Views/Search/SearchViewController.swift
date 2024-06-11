351838d1832fc88358c88fe23a5d55c888175d9e
jinwoong <baseball0020@gmail.com>
2024-06-11 21:28:45 +0900

    refactor: SearchView의 의존성을 생성자를 통해 주입

--- TodaysFlowers/Presentation/Views/Search/SearchViewController.swift
+++ TodaysFlowers/Presentation/Views/Search/SearchViewController.swift
@@ -10,7 +10,7 @@ import Combine
 
 final class SearchViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchResultsUpdating {
     
-    private var viewModel = SearchViewModel()
+    private var viewModel: SearchViewModel
     private var cancellables = Set<AnyCancellable>()
     private let monthDayPickerView = MonthDayPickerView()
     
@@ -24,6 +24,15 @@ final class SearchViewController: UIViewController, UITableViewDelegate, UITable
         
         return tableView
     }()
+    
+    init(viewModel: SearchViewModel) {
+        self.viewModel = viewModel
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
